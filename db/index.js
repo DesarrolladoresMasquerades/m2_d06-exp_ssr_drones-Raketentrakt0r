59b937236a7d2867f0ec25495c181d4030837ce8
Luis Moncada <raketentrakt0r@gmail.com>
2022-03-05 16:39:40 +0100

    commit

--- db/index.js
+++ db/index.js
@@ -1,14 +1,15 @@
 // ℹ️ package responsible to make the connection with mongodb
 // https://www.npmjs.com/package/mongoose
+require("dotenv").config()
 const mongoose = require("mongoose");
 
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
+//const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-express-drones";
 
 mongoose
-  .connect(MONGO_URI, {
+  .connect(`mongodb+srv://${process.env.MG_USERNAME}:${process.env.MG_PWD}@cluster0.tz3zj.mongodb.net/The-drones?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,31 @@
 // Iteration #1
+const mongoose = require('mongoose');
+const Drone = require('../models/Drone.model')
+
+require('..db/index.js')
+
+const drones = [
+    {
+    name:"General Atomics MQ-9 Reaper",
+    proppeller:"4",
+    masSpeed:18
+    },
+    {
+    name: "Racer 57", 
+    propellers: 4, 
+    maxSpeed: 20
+    },
+    {
+    name: "Courier 3000i", 
+    propellers: 6, 
+    maxSpeed: 18
+    }
+]
+
+Drone.create(drones)
+  .then(dronesFromDB => {
+
+    return console.log(`Created ${dronesFromDB.length} drone`);
+    
+  })
+  .catch(err => console.log(`An error occurred while creating books: ${err}`));
